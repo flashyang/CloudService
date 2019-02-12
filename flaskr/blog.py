a9453d91ac7fb24859ed0ea067cb1c6117ef3e36
brilliantjc <brilliantjc@126.com>
2019-02-12 11:32:55 -0800

    updated to Apartment interface

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -89,6 +89,32 @@ def update(id):
     return render_template('blog/update.html', post=post)
 
 
+@bp.route('/<int:id>/createNest', methods=('GET', 'POST'))
+@login_required
+def createNest(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required

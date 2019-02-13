d361a93cb15464c36f3e33fa6ca82227d4129980
stan95 <stan95y@gmail.com>
2019-02-13 14:09:39 -0800

    add search method

--- groupnest/apartment.py
+++ groupnest/apartment.py
@@ -8,6 +8,8 @@ from groupnest.db import get_db
 
 bp = Blueprint('apartment', __name__, url_prefix='/apartment')
 
+
+#GET: / Return the index page
 @bp.route('/')
 def index():
     db = get_db()
@@ -16,3 +18,19 @@ def index():
         'FROM db'
         'ORDER BY created DESC'
     ).fetchall()
+
+#GET: /apartment/<int: zipcode>/search (zipcode)
+#Get a list of apartments by searching zipcode
+@bp.route('/<int:id>/search', methods=('GET',))
+def search(zipcode):
+    db = get_db()
+    aprtments = db.execute(
+        'SELECT *'
+        'FROM db'
+        'ORDER BY created DESC'
+        'WHERE zip = ?',
+        (zipcode,)
+    ).fetchall()
+    if aprtments is None:
+        abort(Response("No such zipcode exists in our databse. Sorry! :("))
+    return aprtments

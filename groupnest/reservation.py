912d1c134a129430b964caad55d89f771ee7cc33
qwang3q <qwang3q@gmail.com>
2019-02-13 23:37:36 -0800

    add accept offer in reservation table

--- groupnest/reservation.py
+++ groupnest/reservation.py
@@ -8,6 +8,17 @@ from groupnest.db import get_db
 
 bp = Blueprint('reservation', __name__, url_prefix='/reservation')
 
-@bp.route('/')
-def index():
-    return 'Hello, reservation!'
+@bp.route('/nest_id/<int:nest_id>/create', methods=('GET', 'POST'))
+@login_required
+def create(nest_id):
+    if request.method == 'POST':
+        db = get_db()
+        db.execute(
+            'INSERT INTO reservation (nest_id, tenant_id)'
+            ' VALUES (?, ?)',
+            (nest_id, g.user['user_id'])
+        )
+        db.commit()
+    return redirect(url_for('nest.index', nest_id))
+
+
